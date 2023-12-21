4590cd7234568bd211be164badb2bab9e988a416
Deepak <deepak2603dk@gmail.com>
2023-12-21 19:39:15 +0530

    Bigint type

---
+++ Datatypes/bigInt.js
@@ -0,0 +1,5 @@
+// Number has limit 2^53 = 9007199254740991
+//after that we have to take type bigInt
+var num1 = 9007199254740991;
+console.log('>>>>>>>>>>>', num1);
+
---
+++ Datatypes/bigInt.ts
@@ -0,0 +1,10 @@
+// Number has limit 2^53 = 9007199254740991
+//after that we have to take type bigInt
+
+
+let num1:number = 9007199254740991;
+console.log('>>>>>>>>>>>', num1)
+
+
+let num2:bigint = 922207199254740991n
+console.log('>>>>>>>>>>>', num2)
