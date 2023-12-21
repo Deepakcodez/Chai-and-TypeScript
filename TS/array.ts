133beff69582fd0deec606735b7beb9c01485e18
Deepak <deepak2603dk@gmail.com>
2023-12-21 21:13:38 +0530

    array

---
+++ TS/array.ts
@@ -0,0 +1,8 @@
+
+let arr1:Array<string> = ["js","ts","node","express"]
+console.log('>>>>>>>>>>>', arr1)
+
+// if you know the lenght of array 
+
+let arr2:[number,number,number,string] = [34,56,34,"ts"]
+console.log('>>>>>>>>>>>', arr2)
