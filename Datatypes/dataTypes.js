81fa585e9cde0a60b389bc9e858eb3a331c55d32
Deepak <deepak2603dk@gmail.com>
2023-12-21 19:08:38 +0530

    data types

---
+++ Datatypes/dataTypes.js
@@ -0,0 +1,8 @@
+var a = "Typescript";
+console.log('>>>>>>>>>>>', a);
+var b = 45;
+console.log('>>>>>>>>>>>', b);
+var c = "hero";
+console.log('>>>>>>>>>>> by second method', c);
+var bool = true;
+console.log('>>>>>>>>>>>', bool);
---
+++ Datatypes/dataTypes.ts
@@ -0,0 +1,14 @@
+let a:string = "Typescript";
+console.log('>>>>>>>>>>>',a)
+
+let b:number = 45   
+console.log('>>>>>>>>>>>', b)
+
+
+let c = <string>"hero"
+console.log('>>>>>>>>>>> by second method', c)
+
+let bool:boolean = true;
+console.log('>>>>>>>>>>>', bool)
+
+
